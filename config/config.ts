6702db82a1924b3a3b637210c6b711e31a2d99fa
sagarkhadka12345 <sagar.khadka2001@gmail.com>
2022-06-16 20:20:40 +0545

    fixed package

--- config/config.ts
+++ config/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 
 const jwt_secret = process.env.ACCESS_TOKEN_SECRET || ""
 
-const SERVER_PORT = process.env.SERVER_PORT || 3000;
+const SERVER_PORT = process.env.PORT || 3000;
 const MONGO_USERNAME = process.env.MONGO_USERNAME || ""
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || ""
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.rsh6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
